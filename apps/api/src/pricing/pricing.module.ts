cca2134ca79d541205d592020edf9cd058dad660
Olaniyi Aborisade <olaniyi_g@hotmail.com>
2026-03-21 15:46:08 +0100

    fix(api): import auth module into pricing module

--- apps/api/src/pricing/pricing.module.ts
+++ apps/api/src/pricing/pricing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PricingController } from './pricing.controller';
 import { PricingRepository } from './pricing.repository';
 import { PricingService } from './pricing.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PricingController],
   providers: [PricingRepository, PricingService],
   exports: [PricingService],
